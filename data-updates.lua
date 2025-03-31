6f6fc5eef92af84d7d5a1860b2fce4eace27f864
Lester Lee <contact@lester-lee.com>
2025-03-31 18:24:53 -0400

    Format lua

--- data-updates.lua
+++ data-updates.lua
@@ -1,33 +1,34 @@
 ---@param item data.ItemPrototype
 ---@return data.RecipePrototype?
 local function create_spoilage_recipe(item)
-    if not item.spoil_result then return end
-    return {
-        type = "recipe",
-        name = "spoil-" .. item.name,
-        localised_name = { "", "Spoil ", { "item-name." .. item.name } },
-        ingredients = {
-            { type = "item", name = item.name, amount = 1 }
-        },
-        results = {
-            { type = "item", name = item.spoil_result, amount = 1 }
-        },
-        category = "spoiling",
-        icon = item.icon,
-        icon_size = 64,
-        hide_from_player_crafting = true,
-    }
+	if not item.spoil_result then
+		return
+	end
+	return {
+		type = "recipe",
+		name = "spoil-" .. item.name,
+		localised_name = { "", "Spoil ", { "item-name." .. item.name } },
+		ingredients = {
+			{ type = "item", name = item.name, amount = 1 },
+		},
+		results = {
+			{ type = "item", name = item.spoil_result, amount = 1 },
+		},
+		category = "spoiling",
+		icon = item.icon,
+		icon_size = 64,
+		hide_from_player_crafting = true,
+	}
 end
 
 local function create_spoilage_recipes(source)
-    for _, item in pairs(source) do
-        local recipe = create_spoilage_recipe(item)
-        if recipe then
-            data:extend { recipe }
-        end
-    end
+	for _, item in pairs(source) do
+		local recipe = create_spoilage_recipe(item)
+		if recipe then
+			data:extend({ recipe })
+		end
+	end
 end
-
 
 create_spoilage_recipes(data.raw.item)
 create_spoilage_recipes(data.raw.capsule)
